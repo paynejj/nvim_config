0956550c612b4428893cca5bbac3c73025621cd2
Joshua Payne <70377045+paynejj@users.noreply.github.com>
2024-04-10 16:06:13 -0400

    activate all files

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,4 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
