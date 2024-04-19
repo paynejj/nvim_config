09a9eb8cbea7b3a63e2a515947f276006eb28b69
Joshua Payne <70377045+paynejj@users.noreply.github.com>
2024-04-19 16:54:02 -0400

    add community plugins

--- lua/community.lua
+++ lua/community.lua
@@ -6,39 +6,16 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.editing-support.bigfile-nvim" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   { import = "astrocommunity.motion.mini-surround" },
-  {
-    "echasnovski/mini.surround",
-    opts = {
-      mappings = {
-        add = "sa", -- Add surrounding in Normal and Visual modes
-        delete = "sd", -- Delete surrounding
-        find = "sf", -- Find surrounding (to the right)
-        find_left = "sF", -- Find surrounding (to the left)
-        sighlight = "sh", -- Highlight surrounding
-        replace = "sr", -- Replace surrounding
-        update_n_lines = "sn", -- Update `n_lines`
-      },
-    },
-  },
   { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.full-dadbod" },
   { import = "astrocommunity.pack.java" },
-  {
-    "jay-babu/mason-null-ls.nvim",
-    opts = function(_, opts)
-      for i, v in ipairs(opts.ensure_installed) do
-        if v == "clang_format" then
-          table.remove(opts.ensure_installed, i)
-          break
-        end
-      end
-    end,
-  },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.tailwindcss" },
-  { import = "astrocommunity.editing-support.bigfile-nvim" },
+  { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.terminal-integration.flatten-nvim" },
 }
