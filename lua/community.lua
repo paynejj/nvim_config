1635d13dae873866786673a01cc5c7ed0682f204
Joshua Payne <70377045+paynejj@users.noreply.github.com>
2024-04-23 16:31:31 -0400

    use nvim-surround instead of mini-surround

--- lua/community.lua
+++ lua/community.lua
@@ -8,7 +8,7 @@ return {
   { import = "astrocommunity.colorscheme.rose-pine" },
   { import = "astrocommunity.editing-support.bigfile-nvim" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
-  { import = "astrocommunity.motion.mini-surround" },
+  { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.full-dadbod" },
