8eac02044b8f3ce8880b8101ff842d274a872569
Joshua Payne <70377045+paynejj@users.noreply.github.com>
2025-03-29 09:05:57 -0500

    astronvim v5 reset

--- lua/community.lua
+++ lua/community.lua
@@ -1,3 +1,5 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -5,18 +7,6 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.colorscheme.rose-pine" },
-  { import = "astrocommunity.editing-support.bigfile-nvim" },
-  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
-  { import = "astrocommunity.motion.nvim-surround" },
-  { import = "astrocommunity.pack.bash" },
-  { import = "astrocommunity.pack.docker" },
-  { import = "astrocommunity.pack.full-dadbod" },
-  { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.tailwindcss" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.terminal-integration.flatten-nvim" },
+  -- import/override with your plugins folder
 }
